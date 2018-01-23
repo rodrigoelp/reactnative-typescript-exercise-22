e715cc3f766c4b1fb79f6790907a52c7404d819a
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-23 15:13:01 +1100

    Hooked up the component generated by typescript

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { AppRegistry, View } from "react-native";
+
+class App extends React.Component {
+    public render() {
+        return (<View></View>);
+    }
+}
+
+AppRegistry.registerRunnable("lottieApp", () => App);
