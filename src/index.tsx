c23e777371cec1e14bd40f3a3d9bdfd75f2a88c8
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-25 10:47:23 +1100

    One of the awesome things of declarative animations is that you can switch your target and lottie just continues as if nothing happened. In here we are changing the animations between the coding ape and the react logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
-import { AppRegistry, View, Text, Animated } from "react-native";
+import { AppRegistry, View, Text, Animated, Button, Switch } from "react-native";
 import LottieView = require("lottie-react-native");
+
+const animationOptions = {
+    codingApe: require("./animations/coding_ape.json"),
+    reactLogo: require("./animations/reactlogo.json")
+};
 
 interface AppState {
     animationProgress: Animated.Value;
+    playAnimation: boolean;
+    animation?: Animated.CompositeAnimation;
+
+    animationSource: any;
+    chooseApeOverLogo: boolean;
 }
 
 class App extends React.Component<{}, AppState> {
@@ -11,22 +21,55 @@ class App extends React.Component<{}, AppState> {
         super(props);
 
         this.state = {
-            animationProgress: new Animated.Value(0)
+            animationProgress: new Animated.Value(0),
+            playAnimation: false,
+            chooseApeOverLogo: false,
+            animationSource: animationOptions.reactLogo
         }
-    }
 
-    componentDidMount() {
-        Animated.timing(this.state.animationProgress, { toValue: 1, duration: 3000 })
-            .start();
+        this.toggleAnimation = this.toggleAnimation.bind(this);
+        this.toggleAnimationSource = this.toggleAnimationSource.bind(this);
     }
 
     public render() {
-        const source = require("./animations/coding_ape.json");
         return (
-            <View style={{ flex: 1}}>
-                <LottieView source={source} progress={this.state.animationProgress} />
+            <View style={{ flex: 1, backgroundColor: "black" }}>
+                <LottieView source={this.state.animationSource} progress={this.state.animationProgress} />
+                <View style={{ flexDirection: "row", marginTop: 60, marginHorizontal: 30 }}>
+                    <Switch value={this.state.playAnimation} onValueChange={this.toggleAnimation} />
+                    <Text style={{ color: "white", alignSelf: "center", marginLeft: 16 }}>Toogle the animation</Text>
+                </View>
+                <View style={{ flexDirection: "row", marginTop: 16, marginHorizontal: 30 }}>
+                    <Text style={{ color: "white", alignSelf: "center", marginRight: 16 }}>React Logo</Text>
+                    <Switch value={this.state.chooseApeOverLogo} onValueChange={this.toggleAnimationSource} />
+                    <Text style={{ color: "white", alignSelf: "center", marginLeft: 16 }}>Coding Ape</Text>
+                </View>
             </View>
         );
+    }
+
+    toggleAnimation(play: boolean) {
+        if (play) {
+            const animation = Animated.loop(
+                Animated.timing(this.state.animationProgress, { toValue: 1, duration: 3000 })
+            );
+            this.setState(
+                { ...this.state, animation: animation, playAnimation: play },
+                () => animation.start()
+            );
+        } else {
+            const animation = this.state.animation;
+            if (animation !== undefined) {
+                this.setState(
+                    { ...this.state, animation: undefined, playAnimation: play },
+                    () => animation.stop()
+                );
+            }
+        }
+    }
+
+    toggleAnimationSource(chooseApe: boolean) {
+        this.setState({ ...this.state, chooseApeOverLogo: chooseApe, animationSource: chooseApe ? animationOptions.codingApe : animationOptions.reactLogo });
     }
 }
 
