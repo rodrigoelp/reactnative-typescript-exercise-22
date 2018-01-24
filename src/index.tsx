b1f5d6e988a58ae12a5c319326f1e8e44bc6486b
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-24 20:45:23 +1100

    PARKING THIS.
    Found the airbnb type definitions are wrong and need to be changed to the code as indicated in lottie.typedef.d.ts
    
    Hopefully the pull request is going to be accepted soon.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
-import { AppRegistry, View } from "react-native";
-import LottieView from "lottie-react-native";
+import { AppRegistry, View, Text, Animated } from "react-native";
+import LottieView = require("lottie-react-native");
 
-class App extends React.Component {
+interface AppState {
+    animationProgress: Animated.Value;
+}
+
+class App extends React.Component<{}, AppState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            animationProgress: new Animated.Value(0)
+        }
+    }
+
+    componentDidMount() {
+        Animated.timing(this.state.animationProgress, { toValue: 1, duration: 3000 })
+            .start();
+    }
+
     public render() {
-        return (<View></View>);
+        const source = require("./animations/coding_ape.json");
+        return (
+            <View style={{ flex: 1}}>
+                <LottieView source={source} progress={this.state.animationProgress} />
+            </View>
+        );
     }
 }
 
-AppRegistry.registerRunnable("lottieApp", () => App);
+AppRegistry.registerComponent("lottieApp", () => App);
